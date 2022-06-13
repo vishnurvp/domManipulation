c99787ab6dc5637e407b32181791f63f39ecc106
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-06-13 19:56:02 +0530

    domManipulation1

---
+++ script.js
@@ -0,0 +1,35 @@
+// // examine te document object
+// console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 'new Title';
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// //document.all[10].textContent = 'Hello';
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+// getElementById
+
+// console.log(document.getElementById('header-title'));
+// const headertitle = document.getElementById('header-title');
+// const header = document.getElementById('main-header');
+// console.log(headertitle);
+// headertitle.textContent = 'Hello';
+// headertitle.innerText = 'goodbye';
+
+// header.style.borderBottom = 'solid 3px #000';
+
+const addItems = document.getElementsByClassName('title')[0];
+addItems.style.fontWeight = 'bold';
+addItems.style.color = 'green';
+
+
+
+
