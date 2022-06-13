689b1696af592741574776a4913104ecee8e34bc
Vishnuprasad <vishnuprasad.rvp@icloud.com>
2022-06-13 22:11:19 +0530

    querySelector_querySelectorAll

--- script.js
+++ script.js
@@ -43,24 +43,61 @@
 // }
 
 
-const li = document.getElementsByTagName('li');
-// console.log(items);
+// const li = document.getElementsByTagName('li');
+// // console.log(items);
 
-li[1].textContent = 'Hello 2';
+// li[1].textContent = 'Hello 2';
 
-li[2].style.backgroundColor = 'green';
+// li[2].style.backgroundColor = 'green';
 
-for (let i=0; i<li.length; i++) {
-    li[i].style.fontWeight = 'bold';
-    li[i].style.color = 'red';
+// for (let i=0; i<li.length; i++) {
+//     li[i].style.fontWeight = 'bold';
+//     li[i].style.color = 'red';
+// }
+
+// const a = document.getElementsByClassName('list-group')[0];
+// console.log(a.lastElementChild);
+// a.lastElementChild.textContent = 'Changed ItemName through class name';
+
+// const b = document.getElementsByTagName('li')[4];
+// b.style.backgroundColor ='blue';
+
+const header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+const input = document.querySelector('input');
+input.value = 'hello world';
+
+const submit = document.querySelector("input[type='submit']");
+submit.value = "SEND";
+
+const item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+const lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+const secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+const thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display = 'none';
+
+
+// queryselectorall
+
+const listItems = document.querySelectorAll('.list-group-item');
+listItems[1].style.color = 'green';
+console.log(listItems);
+// console.log(titles);
+let odd = document.querySelectorAll('.list-group-item:nth-child(odd)');
+// let even = document.querySelectorAll('li:nth-child(even)');
+
+for (let i=0; i<odd.length; i++) {
+    odd[i].style.backgroundColor = 'green';
+    // even[i].style.backgroundColor = '#ccc';
+
 }
 
-const a = document.getElementsByClassName('list-group')[0];
-console.log(a.lastElementChild);
-a.lastElementChild.textContent = 'Changed ItemName through class name';
-
-const b = document.getElementsByTagName('li')[4];
-b.style.backgroundColor ='blue';
 
 
 
